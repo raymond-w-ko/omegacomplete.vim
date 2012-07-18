82dd3cd42594a768c631784750f61e70720d9caf
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-07-18 09:56:59 -0400

    wrong regex that doesn't really do anything

--- server/Build/VC100/Libs/clang/generate_lib.bat
+++ server/Build/VC100/Libs/clang/generate_lib.bat
@@ -3,7 +3,7 @@ set PATH=C:\MinGW32\bin;C:\cygwin\bin;%PATH%
 objdump -p C:\clang\bin\clang.dll >objdump.tmp
 grep clang_ objdump.tmp >filtered_exports.tmp
 echo EXPORTS >clang.def
-sed -E "s/^.8?\[.*?\]\s//" filtered_exports.tmp >>clang.def
+sed -E "s/^.*?\[.*?\]\s//" filtered_exports.tmp >>clang.def
 unix2dos clang.def
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 lib /machine:i386 /def:clang.def
