414a7b51a488226187db3eef197ad9f03679f9fd
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-07-18 00:09:10 -0400

    ignore files, update to use .tmp files

--- server/Build/VC100/Libs/clang/generate_lib.bat
+++ server/Build/VC100/Libs/clang/generate_lib.bat
@@ -1,9 +1,9 @@
 pushd %~dp0
 set PATH=C:\MinGW32\bin;C:\cygwin\bin;%PATH%
-objdump -p C:\clang\bin\clang.dll >objdump.txt
-grep clang_ objdump.txt >filtered_exports.txt
+objdump -p C:\clang\bin\clang.dll >objdump.tmp
+grep clang_ objdump.tmp >filtered_exports.tmp
 echo EXPORTS >clang.def
-sed -E "s/^.8?\[.*?\]\s//" filtered_exports.txt >>clang.def
+sed -E "s/^.8?\[.*?\]\s//" filtered_exports.tmp >>clang.def
 unix2dos clang.def
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 lib /machine:i386 /def:clang.def
