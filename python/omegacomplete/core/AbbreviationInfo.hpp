f4e42dfe440aba0799097481ff2e533350688683
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-08-26 22:29:37 -0400

    move utility structs to their own file

---
+++ python/omegacomplete/core/AbbreviationInfo.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+struct AbbreviationInfo
+{
+    AbbreviationInfo(unsigned weight, const std::string word)
+        : Weight(weight), Word(word) { }
+    AbbreviationInfo(const AbbreviationInfo& other)
+        : Weight(other.Weight), Word(other.Word) { }
+    ~AbbreviationInfo() { }
+
+    unsigned Weight;
+    const std::string Word;
+
+    bool operator<(const AbbreviationInfo& other) const
+    {
+        if (Weight < other.Weight)
+            return true;
+        else if (Weight > other.Weight)
+            return false;
+
+        return Word < other.Word;
+    }
+};
---
+++ python/omegacomplete/core/WordInfo.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+struct WordInfo
+{
+    WordInfo()
+    :
+    ReferenceCount(0),
+    GeneratedAbbreviations(false)
+    { }
+
+    ~WordInfo() { }
+
+    int ReferenceCount;
+    bool GeneratedAbbreviations;
+};
