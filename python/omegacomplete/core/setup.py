a66f7538a139b38fe38d9185d447af3b06923744
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-09-07 10:19:13 -0400

    minor formatting

--- python/omegacomplete/core/setup.py
+++ python/omegacomplete/core/setup.py
@@ -1,18 +1,21 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('core',
-                    define_macros = [('MAJOR_VERSION', '1'),
-                                     ('MINOR_VERSION', '0')],
                     libraries = ['boost_thread-mt', 'boost_system-mt'],
-                    sources = ['Algorithm.cpp', 'Buffer.cpp', 'DllMain.cpp', 'GlobalWordSet.cpp', 'LookupTable.cpp', 'OmegaComplete.cpp', 'Participant.cpp', 'PythonMain.cpp', 'Room.cpp', 'stdafx.cpp', 'Tags.cpp', 'TagsSet.cpp', 'Teleprompter.cpp', 'TrieNode.cpp', 'ustring.cpp'])
+                    sources = ['Algorithm.cpp', 'Buffer.cpp', 'DllMain.cpp',
+                        'GlobalWordSet.cpp', 'LookupTable.cpp',
+                        'OmegaComplete.cpp', 'Participant.cpp', 'PythonMain.cpp',
+                        'Room.cpp', 'stdafx.cpp', 'Tags.cpp', 'TagsSet.cpp',
+                        'Teleprompter.cpp', 'TrieNode.cpp', 'ustring.cpp'])
 
-setup (name = 'core',
-       version = '1.0',
-       description = 'OmegaComplete Core',
-       author = 'Raymond W. Ko',
-       author_email = 'raymond.w.ko@gmail.com',
-       url = 'https://github.com/raymond-w-ko/omegacomplete.vim',
-       long_description = '''
-This is the C++ portion of OmegaComplete
+setup(name = 'core',
+      version = '1.0',
+      description = 'OmegaComplete Core',
+      author = 'Raymond W. Ko',
+      author_email = 'raymond.w.ko@gmail.com',
+      url = 'https://github.com/raymond-w-ko/omegacomplete.vim',
+      long_description = '''
+This is the C++ portion of OmegaComplete. It is responsible for parsing buffers
+and calculating completions.
 ''',
-       ext_modules = [module1])
+      ext_modules = [module1])
