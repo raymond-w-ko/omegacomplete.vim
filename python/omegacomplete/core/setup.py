0cee0dac7add776bbac5f591436809dc5b502ea7
Raymond W. Ko <raymond.w.ko@gmail.com>
2012-12-10 15:43:46 -0500

    Makefile stuff for Mac OS X 10.7

--- python/omegacomplete/core/setup.py
+++ python/omegacomplete/core/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 
+# example module setup file for Cygwin and Linux
 module1 = Extension('core',
                     libraries = ['boost_thread-mt', 'boost_system-mt'],
                     sources = ['Algorithm.cpp', 'Buffer.cpp', 'DllMain.cpp',
@@ -7,6 +8,19 @@ module1 = Extension('core',
                         'OmegaComplete.cpp', 'PythonMain.cpp', 'stdafx.cpp',
                         'Tags.cpp', 'TagsSet.cpp', 'TrieNode.cpp',
                         'ustring.cpp'])
+
+# example module setup file for Mac OS X 10.7
+#module1 = Extension('core',
+                    #include_dirs = ['/Users/rko/boost',
+                                    #'/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7',
+                                    #'/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7/'],
+                    #library_dirs = ['/Users/rko/boost/stage/lib'],
+                    #libraries = ['boost_thread', 'boost_system'],
+                    #sources = ['Algorithm.cpp', 'Buffer.cpp', 'DllMain.cpp',
+                        #'GlobalWordSet.cpp', 'LookupTable.cpp',
+                        #'OmegaComplete.cpp', 'PythonMain.cpp', 'stdafx.cpp',
+                        #'Tags.cpp', 'TagsSet.cpp', 'TrieNode.cpp',
+                        #'ustring.cpp'])
 
 setup(name = 'core',
       version = '1.0',
