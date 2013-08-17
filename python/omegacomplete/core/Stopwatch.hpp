291091d07e42561bff66613087ffa3ddb8e957b3
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-08-16 22:25:16 -0400

    switch to using boost::chrono to be cross platform

--- python/omegacomplete/core/Stopwatch.hpp
+++ python/omegacomplete/core/Stopwatch.hpp
@@ -1,78 +1,38 @@
-#ifndef STOPWATCH_HPP
-#define STOPWATCH_HPP
+#pragma once
 
-#ifdef _WIN32
-
-// from
-// http://stackoverflow.com/questions/922829/c-boost-posix-time-elapsed-seconds-elapsed-fractional-seconds
-
-//! \brief Stopwatch for timing performance values
-//!
-//! This stopwatch class is designed for timing performance of various
-//! software operations.  If the values you get back a greater than a
-//! few seconds, you should be using a different tool.
-//! On a Core 2 Duo E6850 @ 3.00GHz, the start/stop sequence takes
-//! approximately 230 nano seconds in the debug configuration and 180
-//! nano seconds in the release configuration.  If you are timing on the
-//! sub-microsecond scale, please take this into account and test it on
-//! your machine.
 class Stopwatch {
 public:
-    //! \param start if set to true will initialize and then start the
-    //! timer.
 	Stopwatch(bool start=false) {
-        _start.QuadPart = 0;
-        _stop.QuadPart = 0;
-        if(start)
+        if (start)
 			Start();
     }
 
-    //! Starts the stopwatch running
 	void Start() {
-		QueryPerformanceCounter(&_start);
+		start_ = boost::chrono::steady_clock::now();
     }
 
-    //! Run this when the event being timed is complete
     void Stop() {
-		QueryPerformanceCounter(&_stop);
+		end_ = boost::chrono::steady_clock::now();
     }
 
 	//! Stops the timer and returns the result
-	double StopResult() {
+	uint64_t StopResult() {
 		Stop();
 		return ResultNanoseconds();
 	}
 
     //! You can get the result of the stopwatch start-stop sequence at
     //! your leisure.
-    double ResultNanoseconds() {
-        LARGE_INTEGER frequency;
-        QueryPerformanceFrequency(&frequency);
-        double cyclesPerNanosecond = static_cast<double>(frequency.QuadPart) / 1000000000.0;
-
-        LARGE_INTEGER elapsed;
-        elapsed.QuadPart = _stop.QuadPart - _start.QuadPart;
-        return elapsed.QuadPart / cyclesPerNanosecond;
+    uint64_t ResultNanoseconds() {
+        boost::chrono::nanoseconds ns = end_ - start_;
+        return ns.count();
     }
 
-    void PrintResultNanoseconds() {
-        std::cout << ResultNanoseconds() << "ns" << std::endl;
-    }
-    void PrintResultMicroseconds() {
-        std::cout << ResultNanoseconds() / 100 << "us" << std::endl;
-    }
-    void PrintResultMilliseconds() {
-        std::cout << ResultNanoseconds() / 100000 << "ms" << std::endl;
-    }
-    void PrintResultSeconds() {
-        std::cout << ResultNanoseconds() / 1000000000 << "s" << std::endl;
+    std::string ResultNanosecondsToString() {
+        return boost::lexical_cast<std::string>(ResultNanoseconds());
     }
 
 private:
-    LARGE_INTEGER _start;
-    LARGE_INTEGER _stop;
+    boost::chrono::steady_clock::time_point start_;
+    boost::chrono::steady_clock::time_point end_;
 };
-
-#endif
-
-#endif //STOPWATCH_HPP
