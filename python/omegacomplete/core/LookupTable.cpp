127e015c699c2b62e77d56bdc6a1a4a6a81a7822
Raymond W. Ko <raymond.w.ko@gmail.com>
2013-08-21 19:47:48 -0400

    formatting

--- python/omegacomplete/core/LookupTable.cpp
+++ python/omegacomplete/core/LookupTable.cpp
@@ -13,38 +13,38 @@ std::vector<char> LookupTable::QuickMatchKey;
 boost::unordered_map<char, unsigned> LookupTable::ReverseQuickMatch;
 
 void LookupTable::InitStatic() {
-    // generate lookup tables
-    for (size_t index = 0; index <= 255; ++index) {
-        LookupTable::IsPartOfWord[index] =
-            LookupTable::isPartOfWord(static_cast<char>(index)) ? 1 : 0;
+  // generate lookup tables
+  for (size_t index = 0; index <= 255; ++index) {
+    LookupTable::IsPartOfWord[index] =
+        LookupTable::isPartOfWord(static_cast<char>(index)) ? 1 : 0;
 
-        LookupTable::IsUpper[index] =
-            LookupTable::isUpper(static_cast<char>(index)) ? 1 : 0;
+    LookupTable::IsUpper[index] =
+        LookupTable::isUpper(static_cast<char>(index)) ? 1 : 0;
 
-        std::string temp(1, static_cast<char>(index));
-        boost::algorithm::to_lower(temp);
-        LookupTable::ToLower[index] = temp[0];
+    std::string temp(1, static_cast<char>(index));
+    boost::algorithm::to_lower(temp);
+    LookupTable::ToLower[index] = temp[0];
 
-        LookupTable::IsNumber[index] =
-            LookupTable::isNumber(static_cast<char>(index)) ? 1 : 0;
-    }
+    LookupTable::IsNumber[index] =
+        LookupTable::isNumber(static_cast<char>(index)) ? 1 : 0;
+  }
 
-    QuickMatchKey.resize(kMaxNumCompletions, ' '),
+  QuickMatchKey.resize(kMaxNumCompletions, ' '),
 
-    QuickMatchKey[0] = '1';
-    QuickMatchKey[1] = '2';
-    QuickMatchKey[2] = '3';
-    QuickMatchKey[3] = '4';
-    QuickMatchKey[4] = '5';
-    QuickMatchKey[5] = '6';
-    QuickMatchKey[6] = '7';
-    QuickMatchKey[7] = '8';
-    QuickMatchKey[8] = '9';
-    QuickMatchKey[9] = '0';
+      QuickMatchKey[0] = '1';
+  QuickMatchKey[1] = '2';
+  QuickMatchKey[2] = '3';
+  QuickMatchKey[3] = '4';
+  QuickMatchKey[4] = '5';
+  QuickMatchKey[5] = '6';
+  QuickMatchKey[6] = '7';
+  QuickMatchKey[7] = '8';
+  QuickMatchKey[8] = '9';
+  QuickMatchKey[9] = '0';
 
-    for (unsigned ii = 0; ii < 10; ++ii) {
-        ReverseQuickMatch[QuickMatchKey[ii]] = ii;
-    }
+  for (unsigned ii = 0; ii < 10; ++ii) {
+    ReverseQuickMatch[QuickMatchKey[ii]] = ii;
+  }
 }
 
 bool LookupTable::isPartOfWord(char c) {
@@ -59,16 +59,14 @@ bool LookupTable::isPartOfWord(char c) {
   return false;
 }
 
-bool LookupTable::isUpper(char c)
-{
-    if (('A' <= c) && (c <= 'Z'))
-        return true;
-    return false;
+bool LookupTable::isUpper(char c) {
+  if (('A' <= c) && (c <= 'Z'))
+    return true;
+  return false;
 }
 
-bool LookupTable::isNumber(char c)
-{
-    if (('0' <= c) && (c <= '9'))
-        return true;
-    return false;
+bool LookupTable::isNumber(char c) {
+  if (('0' <= c) && (c <= '9'))
+    return true;
+  return false;
 }
--- python/omegacomplete/core/LookupTable.hpp
+++ python/omegacomplete/core/LookupTable.hpp
@@ -1,38 +1,38 @@
 #pragma once
 
 class LookupTable : public boost::noncopyable {
-public:
-    static void InitStatic();
+ public:
+  static void InitStatic();
 
-    /// maximum number of completions to return
-    static const unsigned kMaxNumCompletions;
-    /// maximum number of quick match keys
-    static const unsigned kMaxNumQuickMatch;
+  /// maximum number of completions to return
+  static const unsigned kMaxNumCompletions;
+  /// maximum number of quick match keys
+  static const unsigned kMaxNumQuickMatch;
 
-    /// used to find if a given char is part of a "word"
-    static char IsPartOfWord[256];
-    /// used to find if a given char is an uppercase letter
-    static char IsUpper[256];
-    /// the lowercase equivalent of a given char
-    static char ToLower[256];
-    /// used to find if a given char is a number
-    static char IsNumber[256];
+  /// used to find if a given char is part of a "word"
+  static char IsPartOfWord[256];
+  /// used to find if a given char is an uppercase letter
+  static char IsUpper[256];
+  /// the lowercase equivalent of a given char
+  static char ToLower[256];
+  /// used to find if a given char is a number
+  static char IsNumber[256];
 
-    /// Quick Match Keys
-    /// basically a mapping from result number to keyboard key to press
-    /// first result  -> '1'
-    /// second result -> '2'
-    /// third result  -> '3'
-    /// and etc.
-    static std::vector<char> QuickMatchKey;
-    /// the reverse mapping of the above
-    static boost::unordered_map<char, unsigned> ReverseQuickMatch;
+  /// Quick Match Keys
+  /// basically a mapping from result number to keyboard key to press
+  /// first result  -> '1'
+  /// second result -> '2'
+  /// third result  -> '3'
+  /// and etc.
+  static std::vector<char> QuickMatchKey;
+  /// the reverse mapping of the above
+  static boost::unordered_map<char, unsigned> ReverseQuickMatch;
 
-private:
-    /// this is a purely static class
-    LookupTable();
+ private:
+  /// this is a purely static class
+  LookupTable();
 
-    static bool isPartOfWord(char c);
-    static bool isUpper(char c);
-    static bool isNumber(char c);
+  static bool isPartOfWord(char c);
+  static bool isUpper(char c);
+  static bool isNumber(char c);
 };
