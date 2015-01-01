1dfb42f1a910c0bad02c31a657f1d8407b413584
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-12-31 21:52:02 -0500

    remove unnecessary flag

--- python/omegacomplete/core/make.py
+++ python/omegacomplete/core/make.py
@@ -25,7 +25,7 @@ if sys.platform != 'darwin':
     for i in xrange(len(libs)):
       libs[i] = libs[i] + '-mt'
 
-  global_args = ['-std=c++11', '-mtune=native', '-march=native', '-fno-stack-protector', '-fstrict-aliasing', '-Ofast', '-flto']
+  global_args = ['-std=c++11', '-march=native', '-fno-stack-protector', '-fstrict-aliasing', '-Ofast', '-flto']
 
   compile_args = ['-Wall', '-Wno-char-subscripts']
   compile_args.extend(global_args)
